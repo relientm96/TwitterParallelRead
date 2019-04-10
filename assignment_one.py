865637718af225c8e4c758584c2a64210ca51776
Matthew Yong <yongf@student.unimelb.edu.au>
2019-04-10 19:47:19 +1000

    Working map data class

--- assignment_one.py
+++ assignment_one.py
@@ -12,20 +12,31 @@ mapFilePath = "./data/melbGrid.json"
 twitterFilePath = "./data/tinyTwitter.json"
 
 #Class That Holds Map Data
-
 class mapData:
     def __init__(self,dataPath):
-        map_json = json.load(open(dataPath,'r'))
-        #Return a list of 
-        self.data = [j["properties"] for j in map_json["features"]]
-    
+        try: 
+            open(dataPath,'r')
+            map_json = json.load(open(dataPath,'r'))
+            #Return a list of the properties of each grid
+            self.data = [j["properties"] for j in map_json["features"]]
+        except:
+            print("Error in map file path!")    
+            print("Map File path is",dataPath)
+
     def printMap(self):
         print("From Processors:",rank)
         pprint(self.data)
 
+    #Returns the grid's id based on the given coordinates
+    def idFromCoordinates(self,x,y):
+        for grid in self.data:
+            if( x >= grid["xmin"] ) and ( x < grid["xmax"] ) and ( y >= grid["ymin"] ) and ( y < grid["ymax"] ):
+                return grid["id"]
+            
 def main():
+    #Class containing boundaries for each grid and it's methods
     map = mapData(mapFilePath)
-    map.printMap()
+    
 
 if __name__ == "__main__":
     main()
