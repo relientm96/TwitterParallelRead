501cb0d79c57ff69c68399de7b469bff339d628f
Matthew Yong <yongf@student.unimelb.edu.au>
2019-04-11 10:00:03 +1000

    Added Counter Module for grid counting

--- assignment_one.py
+++ assignment_one.py
@@ -1,5 +1,6 @@
 import sys
 import json
+from collections import Counter
 from pprint import pprint
 from mpi4py import MPI
 
@@ -14,14 +15,10 @@ twitterFilePath = "./data/tinyTwitter.json"
 #Class That Holds Map Data
 class mapData:
     def __init__(self,dataPath):
-        try: 
-            open(dataPath,'r')
+        with open(dataPath,'r',encoding='utf8') as fp:  
             map_json = json.load(open(dataPath,'r'))
             #Return a list of the properties of each grid
             self.data = [j["properties"] for j in map_json["features"]]
-        except:
-            print("Error in map file path!")    
-            print("Map File path is",dataPath)
 
     def printMap(self):
         print("From Processors:",rank)
@@ -30,13 +27,53 @@ class mapData:
     #Returns the grid's id based on the given coordinates
     def idFromCoordinates(self,x,y):
         for grid in self.data:
-            if( x >= grid["xmin"] ) and ( x < grid["xmax"] ) and ( y >= grid["ymin"] ) and ( y < grid["ymax"] ):
+            if( grid["xmin"] <= x <= grid["xmax"] and grid["ymin"] <= y <= grid["ymax"] ) :
                 return grid["id"]
-            
+
+def processOneTweet(map,inputLine):
+    #Remove newline character and , to process json
+    inputLine = inputLine.rstrip(",\n")
+    #Test if legitimate JSON line
+    try:
+        json_txt = json.loads(inputLine)
+    except:
+        #Exit function if invalid json
+        return
+
+    #Check if we have coordinates
+    if (json_txt["doc"]["coordinates"]["coordinates"] is not None):
+        x_input = json_txt["doc"]["coordinates"]["coordinates"][0]
+        y_input = json_txt["doc"]["coordinates"]["coordinates"][1]    
+    return map.idFromCoordinates(x_input,y_input)
+
+def processTwitterData(map,twitterFilePath,grid_counts):
+    with open(twitterFilePath,'r',encoding='utf8') as twitterFile:  
+        line = twitterFile.readline()
+        lineNumber = 1
+        while line:
+            #Distribute line processing using the modulo operation
+            if( lineNumber % comm.Get_size() == rank):
+                id = processOneTweet(map,line)
+                if( id is not None ):
+                    grid_counts[id] += 1
+            line = twitterFile.readline()
+            lineNumber += 1
+    #Gather Results when done
+    final = comm.gather(grid_counts, root=0)
+    pprint(final)    
+
+def printFinalResults(grid_counts):
+    pprint(grid_counts)
+
 def main():
     #Class containing boundaries for each grid and it's methods
     map = mapData(mapFilePath)
-    
+
+    #Store all grid counts here
+    grid_counts = Counter({g["id"] : 0 for g in map.data})
+
+    #Process each line in the twitter file
+    processTwitterData(map,twitterFilePath,grid_counts)
 
 if __name__ == "__main__":
     main()
